d8f9c678ae80f4e4a0593c7c282af17ebf7a49e8
limjung99 <limjung99@naver.com>
2024-01-30 02:58:47 -0800

    Feat : adding flag symbols and change directories

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module hello
+module main
 
 go 1.21.4
 
-require github.com/google/gopacket v1.1.19 // indirect
+require (
+	github.com/elastic/go-licenser v0.4.1 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"runtime"
+)
+
+func defaultInterface() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return "en0"
+	case "windows":
+		return "Ethernet"
+	case "linux":
+		return "ens33"
+	}
+	return "eth0"
+}
+
+var (
+	flagInterface      = flag.String("i", defaultInterface(), "NetworkInterface")
+	flagHelp           = flag.Bool("h", false, "Help and describe what program it is.")
+	flagListInterfaces = flag.Bool("l", false, "LIst available interfaces and exit.")
+)
+
+func main() {
+	flag.Parse()
+
+	if *flagHelp {
+		fmt.Println("Program for ARP spoof in your local net.")
+		fmt.Println("You can spoof other devices on local net.")
+		fmt.Println("You could punished if you use this program,,,")
+		fmt.Println("Be careful!")
+		fmt.Println("	-i : You can define your network interface to spoof local network.")
+		fmt.Println("	-l : list up your all network interface hardware and it's mac address.")
+	}
+
+	if *flagListInterfaces {
+		ifaces, err := net.Interfaces()
+
+		if err != nil {
+			log.Fatal("Failed to show-up interfaces :", err)
+		}
+
+		for idx, iface := range ifaces {
+			if iface.HardwareAddr == nil {
+				continue
+			}
+			fmt.Printf("%d interface \n", idx)
+			fmt.Printf("	MAC Address : %s \n", iface.HardwareAddr)
+			fmt.Printf("	Interface Name : %s \n", iface.Name)
+		}
+
+		os.Exit(0)
+	}
+
+	/*
+		TODO
+			1. List up all host machines name and write it to standard output
+			2. User can select an NIC from information
+			3. Get an gateway IP and MAC address ( but how ? )
+			4. Spoof gateway
+			5. If possible , relay packets in local network.
+	*/
+
+}
